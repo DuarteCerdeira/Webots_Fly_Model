ce9c80264b7e37fa74a7d225af9d9b621c3e8a51
DuarteCerdeira <duarte.cerdeira@tecnico.ulisboa.pt>
2021-07-05 10:44:35 +0100

    Initial Commit

---
+++ controllers/leg_movement/build/release/leg_movement.d
@@ -0,0 +1,5 @@
+build/release/leg_movement.o: leg_movement.c \
+ C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/motor.h
---
+++ controllers/leg_movement/leg_movement.c
@@ -0,0 +1,72 @@
+/*
+ * File:          leg_movement.c
+ * Date:
+ * Description:
+ * Author:
+ * Modifications:
+ */
+
+/*
+ * You may need to add include files like <webots/distance_sensor.h> or
+ * <webots/motor.h>, etc.
+ */
+#include <webots/robot.h>
+#include <webots/motor.h>
+
+/*
+ * You may want to add macros here.
+ */
+#define TIME_STEP 64
+
+/*
+ * This is the main program.
+ * The arguments of the main function can be specified by the
+ * "controllerArgs" field of the Robot node
+ */
+int main(int argc, char **argv) {
+  /* necessary to initialize webots stuff */
+  wb_robot_init();
+  
+  /*
+   * You should declare here WbDeviceTag variables for storing
+   * robot devices like this:
+   *  WbDeviceTag my_sensor = wb_robot_get_device("my_sensor");
+   *  WbDeviceTag my_actuator = wb_robot_get_device("my_actuator");
+   */
+  int i;
+  
+  WbDeviceTag legs[4];
+  char legs_names[4][3] = {"l1", "l2", "l3", "l4"};
+  
+  for (i = 0; i < 4; i++) {
+    legs[i] = wb_robot_get_device(legs_names[i]);
+    wb_motor_set_position(legs[i], INFINITY);
+    wb_motor_set_velocity(legs[i], 1);
+  }
+
+  /* main loop
+   * Perform simulation steps of TIME_STEP milliseconds
+   * and leave the loop when the simulation is over
+   */
+  while (wb_robot_step(TIME_STEP) != -1) {
+    /*
+     * Read the sensors :
+     * Enter here functions to read sensor data, like:
+     *  double val = wb_distance_sensor_get_value(my_sensor);
+     */
+
+    /* Process sensor data here */
+
+    /*
+     * Enter here functions to send actuator commands, like:
+     * wb_motor_set_position(my_actuator, 10.0);
+     */    
+  };
+
+  /* Enter your cleanup code here */
+
+  /* This is necessary to cleanup webots resources */
+  wb_robot_cleanup();
+
+  return 0;
+}
