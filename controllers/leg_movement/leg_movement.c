e00eead3cf103919b1e8c305c91217362361964d
DuarteCerdeira <duarte.cerdeira@tecnico.ulisboa.pt>
2021-07-07 10:41:33 +0100

    Second Prototype

--- controllers/leg_movement/leg_movement.c
+++ controllers/leg_movement/leg_movement.c
@@ -18,6 +18,10 @@
  */
 #define TIME_STEP 64
 
+#define PI 3.14159
+
+enum State {stroke1, stroke2};
+
 /*
  * This is the main program.
  * The arguments of the main function can be specified by the
@@ -33,14 +37,15 @@ int main(int argc, char **argv) {
    *  WbDeviceTag my_sensor = wb_robot_get_device("my_sensor");
    *  WbDeviceTag my_actuator = wb_robot_get_device("my_actuator");
    */
+  int elapsed = 0;
   int i;
+  enum State gait_state = stroke1;
   
   WbDeviceTag legs[4];
   char legs_names[4][3] = {"l1", "l2", "l3", "l4"};
   
   for (i = 0; i < 4; i++) {
     legs[i] = wb_robot_get_device(legs_names[i]);
-    wb_motor_set_position(legs[i], INFINITY);
     wb_motor_set_velocity(legs[i], 1);
   }
 
@@ -49,6 +54,8 @@ int main(int argc, char **argv) {
    * and leave the loop when the simulation is over
    */
   while (wb_robot_step(TIME_STEP) != -1) {
+    elapsed++;
+    gait_state = (elapsed / 25) % 2;
     /*
      * Read the sensors :
      * Enter here functions to read sensor data, like:
@@ -60,7 +67,18 @@ int main(int argc, char **argv) {
     /*
      * Enter here functions to send actuator commands, like:
      * wb_motor_set_position(my_actuator, 10.0);
-     */    
+     */
+    if (gait_state == stroke2) {
+      wb_motor_set_position(legs[0], -2*PI/12);
+      wb_motor_set_position(legs[1], 2*PI/12);
+      wb_motor_set_position(legs[2], -2*PI/12);
+      wb_motor_set_position(legs[3], 2*PI/12);
+    } else if (gait_state == stroke1) {
+      wb_motor_set_position(legs[0], 2*PI/12);
+      wb_motor_set_position(legs[1], -2*PI/12);
+      wb_motor_set_position(legs[2], 2*PI/12);
+      wb_motor_set_position(legs[3], -2*PI/12);
+    }
   };
 
   /* Enter your cleanup code here */
