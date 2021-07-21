8fdb0a99509a57ca38b397dab86a7ec033bf9df5
DuarteCerdeira <duarte.cerdeira@tecnico.ulisboa.pt>
2021-07-21 17:03:45 +0100

    Tweaks to fly model Oscilator controller functional

--- controllers/oscillator_leg_controller/oscillator_leg_controller.c
+++ controllers/oscillator_leg_controller/oscillator_leg_controller.c
@@ -1,4 +1,4 @@
-/*
+  /*
  * File:          oscillator_leg_controller.c
  * Date:
  * Description:
@@ -12,6 +12,8 @@
 #include <webots/motor.h>
 
 #define TIME_STEP 64
+
+#define PI 3.14159265359
 
 #define N_LEG_JOINTS 6
 
@@ -51,27 +53,6 @@ static const char *rr_joints[N_LEG_JOINTS] = {
   "rr_ct_flex/ext" , "rr_ft_flex/ext" , "rr_tt_flex/ext"
 };
 
-void initial_position() {
-  const double front_init_pos[N_LEG_JOINTS] = {0.0, 1.3, 0.1, 1.5, -1.0, 0.4};
-  const double mid_init_pos[N_LEG_JOINTS]   = {0.0, 0.0, 0.5, 1.2, -1.0, 0.5};
-  const double rear_init_pos[N_LEG_JOINTS]  = {0.0, -1.0, 0.8, 0.8, -0.7, 0.2};
-  
-  for (int i = 0; i < N_LEG_JOINTS; i++) {
-    wb_motor_set_position(lf_leg[i], front_init_pos[i]);
-    wb_motor_set_velocity(lf_leg[i], 5.0);
-    wb_motor_set_position(rf_leg[i], front_init_pos[i]);
-    wb_motor_set_velocity(rf_leg[i], 5.0);
-    wb_motor_set_position(lm_leg[i], mid_init_pos[i]);
-    wb_motor_set_velocity(lm_leg[i], 5.0);
-    wb_motor_set_position(rm_leg[i], mid_init_pos[i]);
-    wb_motor_set_velocity(rm_leg[i], 5.0);
-    wb_motor_set_position(lr_leg[i], rear_init_pos[i]);
-    wb_motor_set_velocity(lr_leg[i], 5.0);
-    wb_motor_set_position(rr_leg[i], rear_init_pos[i]);
-    wb_motor_set_velocity(rr_leg[i], 5.0);
-  }
-}
-
 double amp(const double upper_limit, const double lower_limit) {
   return (upper_limit - lower_limit) / 2;
 }
@@ -80,24 +61,24 @@ double average(const double upper_limit, const double lower_limit) {
   return (lower_limit + upper_limit) / 2;
 }
 
-double oscillator(const double upper_limit, const double lower_limit, const int phase, int x) {
-  return amp(upper_limit, lower_limit) * sin(x + phase) + average(upper_limit, lower_limit);
+double oscillator(const double upper_limit, const double lower_limit, const double phase, double x) {
+  return amp(upper_limit, lower_limit) * cos(x + phase) + average(upper_limit, lower_limit);
 }
 
 int main(int argc, char **argv) {
   wb_robot_init();
 
-  const double front_upper_limits[N_LEG_JOINTS] = {0.0, 1.4, 0.2, 3.0, -0.6, 0.4};
-  const double front_lower_limits[N_LEG_JOINTS] = {0.0, 1.2, -0.7, 1.5, -2.4, 0.4};
-  const int front_phases[N_LEG_JOINTS]          = {0, 0, 0, 90, -90, 0};
+  const double front_upper_limits[N_LEG_JOINTS] = {0.2, 1.2, 0.4, 2.2, -1.0, 0.2};
+  const double front_lower_limits[N_LEG_JOINTS] = {-0.2, 0.8, 0.0, 1.4, -2.2, 0.2};
+  const int front_phases[N_LEG_JOINTS]          = {0, 0, PI/2, PI, 0, 0};
 
-  const double middle_upper_limits[N_LEG_JOINTS] = {0.3, 0.4, 0.4, 1.9, -1.4, 0.5};
-  const double middle_lower_limits[N_LEG_JOINTS] = {-0.3, -0.5, 0.3, 1.6, -1.9, 0.5};
-  const int middle_phases[N_LEG_JOINTS]          = {0, 0, -90, 90, 90, 0};
+  const double middle_upper_limits[N_LEG_JOINTS] = {0.0, 0.2, 0.2, 1.6, -1.2, 0.5};
+  const double middle_lower_limits[N_LEG_JOINTS] = {0.0, -0.4, 0.0, 1.4, -1.4, 0.5};
+  const int middle_phases[N_LEG_JOINTS]          = {0, 0, PI/2, 0, PI, 0};
 
-  const double rear_upper_limits[N_LEG_JOINTS] = {0.0, -0.7, 1.3, 1.9, -0.9, 0.2};
-  const double rear_lower_limits[N_LEG_JOINTS] = {0.0, -1.3, 0.7, 0.7, -2.5, 0.2};
-  const int rear_phases[N_LEG_JOINTS]          = {0, 0, 0, 90, -90, 0};
+  const double rear_upper_limits[N_LEG_JOINTS] = {0.2, -1.0, 0.0, 2.6, -0.8, 0.4};
+  const double rear_lower_limits[N_LEG_JOINTS] = {-0.8, -0.8, -0.6, 0.6, -2.2, 0.2};
+  const int rear_phases[N_LEG_JOINTS]          = {0, 0, PI, 0, PI, 0};
 
   for (int i = 0; i < N_LEG_JOINTS; i++) {
     lf_leg[i] = wb_robot_get_device(lf_joints[i]);
@@ -108,22 +89,21 @@ int main(int argc, char **argv) {
     rr_leg[i] = wb_robot_get_device(rr_joints[i]);
   }
 
-  initial_position();
-
   int step = 0;
   while (wb_robot_step(TIME_STEP) != -1) {
     step++;
+    if (step < 1.0 * 1000 / TIME_STEP)
+      continue;
     for (int i = 0; i < N_LEG_JOINTS; i++) {
-      wb_motor_set_position(lf_leg[i], oscillator(front_upper_limits[i], front_lower_limits[i], front_phases[i], step * TIME_STEP));
-      wb_motor_set_position(rf_leg[i], oscillator(front_upper_limits[i], front_lower_limits[i], front_phases[i] + 180, step * TIME_STEP));
+      wb_motor_set_position(lf_leg[i], oscillator(front_upper_limits[i], front_lower_limits[i], front_phases[i], 0.3 * (step * TIME_STEP)));
+      wb_motor_set_position(rf_leg[i], oscillator(front_upper_limits[i], front_lower_limits[i], front_phases[i] + PI, 0.3 * (step * TIME_STEP)));
 
-      wb_motor_set_position(lm_leg[i], oscillator(middle_upper_limits[i], middle_lower_limits[i], middle_phases[i] + 180, step * TIME_STEP));
-      wb_motor_set_position(rm_leg[i], oscillator(middle_upper_limits[i], middle_lower_limits[i], middle_phases[i], step * TIME_STEP));
+      wb_motor_set_position(lm_leg[i], oscillator(middle_upper_limits[i], middle_lower_limits[i], middle_phases[i] + PI, 0.3 * (step * TIME_STEP)));
+      wb_motor_set_position(rm_leg[i], oscillator(middle_upper_limits[i], middle_lower_limits[i], middle_phases[i], 0.3 * (step * TIME_STEP)));
 
-      wb_motor_set_position(lr_leg[i], oscillator(rear_upper_limits[i], rear_lower_limits[i], rear_phases[i], step * TIME_STEP));
-      wb_motor_set_position(rr_leg[i], oscillator(rear_upper_limits[i], rear_lower_limits[i], rear_phases[i] + 180, step * TIME_STEP));
+      wb_motor_set_position(lr_leg[i], oscillator(rear_upper_limits[i], rear_lower_limits[i], rear_phases[i], 0.3 * (step * TIME_STEP)));
+      wb_motor_set_position(rr_leg[i], oscillator(rear_upper_limits[i], rear_lower_limits[i], rear_phases[i] + PI, 0.3 * (step * TIME_STEP)));
     }
-    
   };
 
   wb_robot_cleanup();
