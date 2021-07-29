94190f0308105c8b10120ebd0bc70effc7330d25
DuarteCerdeira <duarte.cerdeira@tecnico.ulisboa.pt>
2021-07-29 15:09:13 +0100

    Eliminated old controllers and world files

--- controllers/leg_movement/build/release/leg_movement.d
+++
@@ -1,5 +0,0 @@
-build/release/leg_movement.o: leg_movement.c \
- C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
- C:\Program\ Files\Webots/include/controller/c/webots/types.h \
- C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
- C:\Program\ Files\Webots/include/controller/c/webots/motor.h
--- controllers/legs_controller/build/release/legs_controller.d
+++
@@ -1,5 +0,0 @@
-build/release/legs_controller.o: legs_controller.c \
- C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
- C:\Program\ Files\Webots/include/controller/c/webots/types.h \
- C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
- C:\Program\ Files\Webots/include/controller/c/webots/motor.h
